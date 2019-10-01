67db0dce383d252c9a676b618ea838f3e96acf7b
Phill Tran <web@philltran.com>
2019-09-30 23:23:33 -0400

    Init commit.

---
+++ throw_shade.py
@@ -0,0 +1,148 @@
+# Copyright 2019 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+# 
+# You may not use this file except in compliance with the terms and conditions 
+# set forth in the accompanying LICENSE.TXT file.
+#
+# THESE MATERIALS ARE PROVIDED ON AN "AS IS" BASIS. AMAZON SPECIFICALLY DISCLAIMS, WITH 
+# RESPECT TO THESE MATERIALS, ALL WARRANTIES, EXPRESS, IMPLIED, OR STATUTORY, INCLUDING 
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT.
+
+import time
+import logging
+import json
+import random
+import threading
+
+from enum import Enum
+from agt import AlexaGadget
+
+from ev3dev2.led import Leds
+from ev3dev2.sound import Sound
+from ev3dev2.motor import OUTPUT_A, SpeedPercent, Motor
+
+# Set the logging level to INFO to see messages from AlexaGadget
+logging.basicConfig(level=logging.INFO)
+
+
+class Direction(Enum):
+    """
+    The list of directional commands and their variations.
+    These variations correspond to the skill slot values.
+    """
+    FORWARD = ['forward', 'forwards', 'go forward', 'lower']
+    BACKWARD = ['back', 'backward', 'go backward', 'raise']
+    STOP = ['stop', 'brake']
+
+
+class Command(Enum):
+    """
+    The list of preset commands and their invocation variation.
+    These variations correspond to the skill slot values.
+    """
+    LOWER_BLIND = ['circle', 'spin']
+    RAISE_BLIND = ['square']
+
+
+class MindstormsGadget(AlexaGadget):
+    """
+    A Mindstorms gadget that performs movement based on voice commands.
+    Two types of commands are supported, directional movement and preset.
+    """
+
+    def __init__(self):
+        """
+        Performs Alexa Gadget initialization routines and ev3dev resource allocation.
+        """
+        super().__init__()
+
+        # Ev3dev initialization
+        self.leds = Leds()
+        self.sound = Sound()
+
+    def on_connected(self, device_addr):
+        """
+        Gadget connected to the paired Echo device.
+        :param device_addr: the address of the device we connected to
+        """
+        self.leds.set_color("LEFT", "GREEN")
+        self.leds.set_color("RIGHT", "GREEN")
+        print("{} connected to Echo device".format(self.friendly_name))
+
+    def on_disconnected(self, device_addr):
+        """
+        Gadget disconnected from the paired Echo device.
+        :param device_addr: the address of the device we disconnected from
+        """
+        self.leds.set_color("LEFT", "BLACK")
+        self.leds.set_color("RIGHT", "BLACK")
+        print("{} disconnected from Echo device".format(self.friendly_name))
+
+    def on_custom_mindstorms_gadget_control(self, directive):
+        """
+        Handles the Custom.Mindstorms.Gadget control directive.
+        :param directive: the custom directive with the matching namespace and name
+        """
+        try:
+            payload = json.loads(directive.payload.decode("utf-8"))
+            print("Control payload: {}".format(payload))
+            control_type = payload["type"]
+            if control_type == "move":
+
+                # Expected params: [direction, duration, speed]
+                self._move(payload["direction"], int(payload["duration"]), int(payload["speed"]))
+
+            if control_type == "command":
+                # Expected params: [command]
+                self._activate(payload["command"])
+
+        except KeyError:
+            print("Missing expected parameters: {}".format(directive))
+
+    def _move(self, direction, duration: int, speed: int, is_blocking=False):
+        """
+        Handles move commands from the directive.
+        Right and left movement can under or over turn depending on the surface type.
+        :param direction: the move direction
+        :param duration: the duration in seconds
+        :param speed: the speed percentage as an integer
+        :param is_blocking: if set, motor run until duration expired before accepting another command
+        """
+        print("Move command: ({}, {}, {}, {})".format(direction, speed, duration, is_blocking))
+        if direction in Direction.FORWARD.value:
+            self.drive.on_for_seconds(SpeedPercent(speed), SpeedPercent(speed), duration, block=is_blocking)
+
+        if direction in Direction.BACKWARD.value:
+            self.drive.on_for_seconds(SpeedPercent(-speed), SpeedPercent(-speed), duration, block=is_blocking)
+
+        if direction in Direction.STOP.value:
+            self.drive.off()
+            self.patrol_mode = False
+
+    def _activate(self, command, speed=50):
+        """
+        Handles preset commands.
+        :param command: the preset command
+        :param speed: the speed if applicable
+        """
+        print("Activate command: ({}, {})".format(command, speed))
+        if command in Command.LOWER_BLIND.value:
+            self.drive.on_for_seconds(SpeedPercent(int(speed)), SpeedPercent(5), 12)
+
+        if command in Command.RAISE_BLIND.value:
+            self.drive.on_for_seconds(SpeedPercent(int(-speed)), SpeedPercent(5), 12)
+
+if __name__ == '__main__':
+
+    # Startup sequence
+    gadget = MindstormsGadget()
+    gadget.sound.play_song((('C4', 'e'), ('D4', 'e'), ('E5', 'q')))
+    gadget.leds.set_color("LEFT", "GREEN")
+    gadget.leds.set_color("RIGHT", "GREEN")
+
+    # Gadget main entry point
+    gadget.main()
+
+    # Shutdown sequence
+    gadget.sound.play_song((('E5', 'e'), ('C4', 'e')))
+    gadget.leds.set_color("LEFT", "BLACK")
+    gadget.leds.set_color("RIGHT", "BLACK")
